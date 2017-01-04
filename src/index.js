97f9bf9410aaa56a62e71ccc650fdfec621a5e14
Ketus-home <maciej.oleksa@gmail.com>
2017-01-04 12:10:37 +0100

    add redux, main reducer, coursesPage component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-//console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import storeConfig from './store/storeConfig';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const store = storeConfig();
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
+    document.getElementById('app')
+);
